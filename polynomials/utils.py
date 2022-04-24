380169e63a27c8c09fd660e93d748133d35105fc
Jiajie Li <ljjsalt@gmail.com>
2022-04-24 04:56:17 +0000

    Remove unused file

--- polynomials/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def crange(coeffs, q):
-    coeffs = np.where((coeffs >= 0) & (coeffs <= q//2),
-                      coeffs,
-                      coeffs - q)
-
-    return coeffs
